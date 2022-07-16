37954c525734c639b83c614c2f36ce4b792573a7
FlimothyCrow <timshogren@gmail.com>
2022-07-15 18:38:20 -0600

    added test to nameCleaner

--- fileSorter.py
+++ fileSorter.py
@@ -8,8 +8,18 @@ def nameCleaner(strToClean):
     extensionStr = os.path.splitext(strToClean)[1]
     return re.sub(r'\W+', '', filename).replace('_1', '') + extensionStr
 
-def createDirs(targetDir, arrayOfStrings):
 
+def directorySanitizer(dirString):
+    counter = 0
+    for file in os.listdir(dirString):
+        counter += 1
+        src = dirString + "/" + file
+        dst = dirString + "/" + nameCleaner(file)
+        os.rename(src, dst)
+
+
+
+def createDirs(targetDir, arrayOfStrings):
     for dirString in arrayOfStrings:
         # loop through arrayOfStrings
         newDirectory = targetDir + "/" + dirString
@@ -21,11 +31,13 @@ def createDirs(targetDir, arrayOfStrings):
         else:
             print("SKIPPING {}".format(newDirectory))
 
+
 def fileMatcher(targetString, substring):
     if substring.lower() in targetString.lower():
         return True
     else:
         return False
+
 
 def flattenDir(directory):
     for dirpath, _, filenames in os.walk(directory, topdown=False):
@@ -90,6 +102,8 @@ def sorterController(targetDir, listOfTerms, flattenBoolean):
     if flattenBoolean:
         flattenDir(targetDir)
     # recursively move all files into targetDir and delete folders
+    directorySanitizer(targetDir)
+    # loops through flattened directory and runs each file through nameCleaner()
     createDirs(targetDir, filteredListOfTerms)
     # create new folders based on search words array
     matchedFiles = matchFinder(os.listdir(targetDir), filteredListOfTerms)
@@ -103,11 +117,11 @@ def sorterController(targetDir, listOfTerms, flattenBoolean):
     print("total time elapsed: " + str(end - start))
     print("directories created: " + str(len(filteredListOfTerms)))
 
-# targetDirectory = 'C:/Users/Timothy/Desktop/TheCrow/target directory'
+targetDirectory = 'C:/Users/Timothy/Desktop/TheCrow/target directory'
 # searchWords = ["ambush", "mummy", "cat's", "mummy"]
 # sorterController(targetDirectory, searchWords, True)
 # flattenDir(targetDirectory)
-
+# directorySanitizer(targetDirectory)
 
 # TO DO set up helper to map through files with regex BEFORE copying
 # TO DO create new folders up one directory, or move them at the end
--- fileSortertest.py
+++ fileSortertest.py
@@ -12,6 +12,8 @@ class sorter(unittest.TestCase):
     def test_nameCleaner(self):
         self.assertEqual("things.jpg", nameCleaner("things///---*&%@#_1_1.jpg"))
         self.assertEqual("cheese.jpeg", nameCleaner("cheese///---*&%@#_1_1.jpeg"))
+        self.assertEqual("waffle.gif", nameCleaner("waffle///.---*&%@#_1_1.gif"))
+        # self.assertEqual("eat at joe's.mp3", nameCleaner("eat at joe's %%#_1.mp3"))
 
     def test_matchFinder(self):
         self.assertEqual({"cheese": ["cheese", "cheese55"]}, matchFinder(
